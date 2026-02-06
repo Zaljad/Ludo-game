e52459494ce4b3d7f6c916e736a38dcb05557383
Zaljad <zaina339966@gmail.com>
2026-02-06 13:17:34 +0300

    complete the board shape

--- script.js
+++ script.js
@@ -1,37 +1,88 @@
+const board = document.querySelector(".game_board")
+const currentPlayer =document.querySelector('#current-player')
 
-const board = document.querySelector('.game_board')
+for (let i = 1; i <= 44; i++) {
+  const cell = document.createElement("div")
+  cell.classList.add("board_cells")
 
-for(let i =1; i<= 40; i++){
-const  cell= document.createElement('div')
-cell.classList.add('board_cells')
-
-  let row, col;
-
-  if(i<=11){
-    row =1; col =i;
+  let row, col
+  //top row
+  if (i <= 12) {
+    row = 1
+    col = i
   }
-  else if(i<=21){
-    row =i -10; col =11;
+  //right col
+   else if (i <= 22) {
+    row = i - 11
+    col = 12
   }
-  else if(i <=31){
-    row =11; col=11-(i-21);
+  // bottom row
+  else if (i <= 34) {
+    row = 12
+    col = 12 - (i - 23)
   }
-  else{
-    row = 11 -(i -31); col =1}
+  //lift col
+  else {
+    row = 11 - (i - 35)
+    col = 1
+  }
 
+  cell.style.gridRow = row
+  cell.style.gridColumn = col
 
-
-cell.style.gridRow = row;
-cell. style.gridColumn = col;
-
-cell.innerText=i;
-if(i%10 ===1) {
-  cell.style.backgroundColor="#00ffff";}
-board.appendChild(cell);
+  cell.innerText = ''
+  if (i===1 || i ===12 || i===23|| i===34) {
+    cell.style.backgroundColor = "#008fec"
+  }
+  if (i === 17 ) {
+    cell.style.backgroundColor = "#fc4d64"
+  }
+  if (i === 40 ) {
+    cell.style.backgroundColor = "#f3ad38"
+  }
+  board.appendChild(cell)
 }
+
+for (let i = 2; i <= 6; i++) {
+  const safeCell = document.createElement("div")
+  safeCell.classList.add("board_cells", "yellow_safe")
+
+  safeCell.style.gridRow = 6
+  safeCell.style.gridColumn = i
+
+  safeCell.style.backgroundColor = "#f3ad38"
+  safeCell.innerText = ""
+
+  board.appendChild(safeCell)
+}
+
+for (let i = 7; i <= 11; i++) {
+  const safeCell = document.createElement("div")
+  safeCell.classList.add("board_cells", "red_safe")
+
+  safeCell.style.gridRow = 6
+  safeCell.style.gridColumn = i
+
+  safeCell.style.backgroundColor = "#fc4d64"
+  safeCell.innerText = ""
+
+  board.appendChild(safeCell)
+}
+
+const yellowCenter = document.createElement('div')
+yellowCenter.classList.add('yellow-center')
+yellowCenter.style.gridRow='5/8'
+yellowCenter.style.gridColumn='6/7'
+board.appendChild(yellowCenter)
+
+const redCenter = document.createElement('div')
+redCenter.classList.add('red-center')
+redCenter.style.gridRow='5/8'
+redCenter.style.gridColumn='7/8'
+board.appendChild(redCenter)
+
 
 /* const enterBtn = document.querySelector('#enter-btn')
 enterBtn.addEventListener('click',()=>{
   window.location.href = "ludo_board.html"
 }) */
-
